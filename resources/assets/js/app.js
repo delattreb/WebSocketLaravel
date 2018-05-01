6d1cb1f3fc0f7c45b8dbfcc4da56ec15b9d727f7
Bruno DELATTRE <delattreb@gmail.com>
2018-05-01 21:45:49 +0200

    patch port

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,15 @@
-require('./bootstrap');
+require('./bootstrap')
 
 import Echo from 'laravel-echo'
 
 let e = new Echo({
     broadcaster: 'socket.io',
     host: window.location.hostname + ':6001'
-});
+})
 
 e.channel('chan-demo').listen('PostCreatedEvent', (e) => {
-    console.log('Hello', e);
-});
+    console.log(e);
+})
 
 
 
