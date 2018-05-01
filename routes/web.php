7e752289004a78ba3163bc7ab8d3c7ef79428d65
Bruno DELATTRE <delattreb@gmail.com>
2018-05-01 14:33:26 +0200

    Add auth

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('welcome');
-});
-
 Route::get('/post', 'PostController@index');
 
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
