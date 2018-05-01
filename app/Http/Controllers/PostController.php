a6049309ab828db659ccc5cd6d71abc5509b0dad
Bruno DELATTRE <delattreb@gmail.com>
2018-05-01 12:10:36 +0200

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\PostCreatedEvent;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $event = new PostCreatedEvent();
+        event($event);
+        dd();
+    }
+}
